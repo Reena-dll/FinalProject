964d9b3d2bca1bc4fbfe489590429f0d53ebf16f
Reena-dll <ouz.spy@gmail.com>
2022-07-15 16:02:07 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine("Kategori ID'e Göre Getir****************");
+            foreach (Product product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("Fiyat Aralığına Göre Getir****************");
+            foreach (Product product in productManager.GetByUnitPrice(1,10))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,81 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                // Oracle, Sql Server, Postgres, MongoDb Simüle ediyoruz.
+                new Product{ProductId = 1, CategoryId = 1, ProductName ="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId = 2, CategoryId = 1, ProductName ="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId = 3, CategoryId = 2, ProductName ="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId = 4, CategoryId = 2, ProductName ="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId = 5, CategoryId = 2, ProductName ="Fare", UnitPrice=85, UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            // LINQ = Language Integrated Query
+            // Lambda =>
+            //Product productToDelete = null;
+            //foreach (Product p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            Product productToDelete = _products.SingleOrDefault(p=> p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            // Gönderdiğim ürün ID'sine sahip olan listede ki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
