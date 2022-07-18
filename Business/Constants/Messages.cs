7a4dc850417d7a9022d40ced25b7da23c47344a4
Reena-dll <ouz.spy@gmail.com>
2022-07-18 19:00:30 +0300

    18 Temmuz Gün Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductListed = "Ürünler Listelendi";
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,15 +12,34 @@ namespace ConsoleUI
         {
             //ProductTest();
             //CategoryTest();
+            //ProductDetails();
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
             {
-                Console.WriteLine("{0} => {1} => {2} => {3}"
-                    ,item.ProductId,item.ProductName,
-                    item.CategoryName,item.UnitsInStock);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName+" "+product.CategoryName );
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
 
             Console.ReadLine();
+        }
+
+        private static void ProductDetails()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetProductDetails().Data)
+            {
+                Console.WriteLine("{0} => {1} => {2} => {3}"
+                    , item.ProductId, item.ProductName,
+                    item.CategoryName, item.UnitsInStock);
+            }
         }
 
         private static void CategoryTest()
@@ -36,19 +55,19 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetAll())
+            foreach (var item in productManager.GetAll().Data)
             {
                 Console.WriteLine(item.ProductName);
             }
 
             Console.WriteLine("Kategori ID'e Göre Getir****************");
-            foreach (Product product in productManager.GetAllByCategoryId(2))
+            foreach (Product product in productManager.GetAllByCategoryId(2).Data)
             {
                 Console.WriteLine(product.ProductName);
             }
 
             Console.WriteLine("Fiyat Aralığına Göre Getir****************");
-            foreach (Product product in productManager.GetByUnitPrice(1, 10))
+            foreach (Product product in productManager.GetByUnitPrice(1, 10).Data)
             {
                 Console.WriteLine(product.ProductName);
             }
