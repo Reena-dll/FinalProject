967ea8d2b348cf23a8216a1db209b6571060b1bf
Reena-dll <ouz.spy@gmail.com>
2022-07-29 16:13:09 +0300

    29 Temmuz Gün Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ namespace Business.Concrete
         {
             // İş kodları
             // Yetkisi var mı?
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour==23)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
