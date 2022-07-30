b264cd62fc5e5de2bebdb116e26eeba13c983c4a
Reena-dll <ouz.spy@gmail.com>
2022-07-30 20:15:24 +0300

    30 Temmuz Gün Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,10 +25,13 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+
+            // business codes
+            // validation
+
+
+            ValidationTool.Validate(new ProductValidator(),product);
+            
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,8 +33,8 @@ namespace WebAPI
             // IoC Container  
             // AOP
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
