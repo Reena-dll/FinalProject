5a04cad7c5c21c5e0b742b0a8c57b27de076965e
Reena-dll <ouz.spy@gmail.com>
2022-07-31 19:36:29 +0300

    31 Temmuz Gün Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -23,6 +24,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
 
@@ -30,7 +32,7 @@ namespace Business.Concrete
             // validation
 
 
-            ValidationTool.Validate(new ProductValidator(),product);
+           
             
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
--- Core/Utilities/Interceptors/MethodInterception.cs
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -1,21 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Castle.DynamicProxy;
 
 namespace Core.Utilities.Interceptors
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
-    {
-        public int Priority { get; set; }
-
-        public virtual void Intercept(IInvocation invocation)
-        {
-
-        }
-    }
-
     public abstract class MethodInterception : MethodInterceptionBaseAttribute
     {
         protected virtual void OnBefore(IInvocation invocation) { }
@@ -44,20 +31,6 @@ namespace Core.Utilities.Interceptors
                 }
             }
             OnAfter(invocation);
-        }
-    }
-
-    public class AspectInterceptorSelector : IInterceptorSelector
-    {
-        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
-        {
-            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
-                (true).ToList();
-            var methodAttributes = type.GetMethod(method.Name)
-                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
-            classAttributes.AddRange(methodAttributes);
-
-            return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
     }
 }
